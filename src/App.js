a3539ce15653ab826293446a250e784798c86c22
Steven Fehr <shfehr@gmail.com>
2023-10-24 15:52:02 -0500

    Two more checks but inputs working now

--- src/App.js
+++ src/App.js
@@ -13,27 +13,28 @@ const errorMessage = {
 }
 
 const initialErrors = {
-  username: '',
-  size: '',
-  specialText: '',
+  orderName: '',
+  // size: '',
+  // specialText: '',
 }
 
 const initialFormValues = {
-  username: '',
+  orderName: '',
   size: '',
   pepperoni: '',
   mushrooms: '',
   onions: '',
-  extracheese:'',
+  extraCheese: '',
   specialText: '',
+  glutenFree: '',
 }
 
-// const userSchema = yup.object().shape({
-//   username : yup
-//   .string()
-//   .trim()
-//   // .required(errorMessage.usernameRequired)
-//   .min(2, errorMessage.usernameMin),
+const userSchema = yup.object().shape({
+  username : yup
+  .string()
+  .trim()
+  // .required(errorMessage.usernameRequired)
+  .min(2, errorMessage.usernameMin),
 //   // 'size-dropdown': yup
 //   // .oneOf(['','small', 'medium', 'large']),
 //   // 'special-text': yup
@@ -47,7 +48,7 @@ const initialFormValues = {
 //   // .boolean(),
 //   // 'extra-cheese':yup
 //   // .boolean()
-// })
+ })
 
 function Home(props) {
   return <h2 style={{ ...style, borderColor: 'red'}}>The Home Screen</h2>
@@ -69,11 +70,167 @@ function Checklist() {
   }
 }
 
+function Order(props) {
+  const [values, setValues] = useState(initialFormValues)
+
+  const onChange = evt => { 
+    // evt.persist();
+    //   validateChange(evt);
+    //   setValues({
+    //     ...values,
+    //     [evt.target.name]:
+    //       evt.target.type === "checkbox" ? evt.target.checked : evt.target.value,
+    //   });
+  let { type, name, value, checked} = evt.target
+  
+  value = type === 'checkbox' ? checked : value
+  // console.log(name, value)
+  setValues({...values, [name]: value })
+  // console.log(evt)
+  
+    // if (evt.target.id === 'name-input') {
+    //   setValues((prevState) =>({...prevState, username: evt.target.value }))
+    //   // validateChange(evt.target)
+    // }
+    // if (evt.target.id === 'size-dropdown') {
+    //   setValues((prevState) =>({...prevState, size: evt.target.value}))
+    // }
+  
+    // if (evt.target.id ==='special-text') {
+    //   setValues((prevState) =>({...prevState, specialText: evt.target.value}))
+    // }
+    setValues({...values, [name]: value })
+  
+    }
+  const onSubmit = evt => {
+    evt.preventDefault()
+    axios.post('https://reqres.in/api/orders', values)
+      .then(response => {
+        console.log(response.data)
+        setValues(initialFormValues)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  return (
+   <div> 
+    <h2 style={{ ...style, borderColor: 'red'}}>Get yourself a pie.</h2>
+    
+    <div>
+      <h2>Build your own pizza here!</h2>
+      {/* <p>{values.username}</p> */}
+      <form  onSubmit={onSubmit} id="pizza-form">
+        <div>
+          <label>Name for order: <br />
+            <input 
+              value = {values.orderName}
+              onChange = {onChange}
+              id="name-input" 
+              name="orderName" 
+              type="text" 
+              placeholder="Type Name" />
+              <br />
+          </label>
+        <div>
+          <label>Size of pizza: <br /></label>
+            <select
+              value = {values.size}
+              onChange = {onChange}
+              id="size-dropdown" 
+              name="size">
+              <option value="">-- Select Size --</option>
+              <option value="small">Small</option>
+              <option value="medium">Medium</option>
+              <option value="large">Large</option>  
+            </select>
+        </div>
+        <div>
+          <label>
+            <input 
+            checked = {values.pepperoni}
+            onChange = {onChange}
+            name = "pepperoni"
+            value = "pepperoni"
+            type="checkbox" />
+            Pepperoni
+          </label>
+          <label>
+            <input
+            checked = {values.mushrooms}
+            onChange = {onChange}
+            name = "mushrooms"
+            value = "mushrooms" 
+            type="checkbox" />
+            Mushrooms
+          </label>
+          <label>
+            <input
+            checked = {values.onions}
+            onChange = {onChange}
+            value = "onions"
+            name = "onions"
+            type="checkbox" />
+            Onions
+          </label>
+          <label>
+            <input
+            checked = {values.extraCheese}
+            onChange = {onChange} 
+            name = "extraCheese"
+            value = "extraCheese"
+            type="checkbox" />
+            Extra Cheese
+          </label> 
+        </div>
+        <div>
+          <label>Special Instructions: <br />
+            <input 
+              value = {values.specialText}
+              onChange = {onChange}
+              id="special-text" 
+              name="specialText" 
+              type="text" 
+              placeholder="Special requests?" 
+              style={{width: '300px', height: "50px"}}/>
+              <br />
+          </label>
+        </div>
+        <div>
+          <fieldset>
+            <legend>Would you like a gluten-free crust:</legend>
+            <label>
+              <input 
+                checked = {values.glutenFree == 'GF YES'}
+                onChange = {onChange}
+                type="radio" 
+                name="glutenFree" 
+                value="GF YES" />
+                Yes
+            </label>
+            <label>
+              <input 
+                checked = {values.glutenFree == 'GF NO'}
+                onChange = {onChange}
+                type="radio" 
+                name="glutenFree" 
+                value="GF NO" />
+                No
+            </label>
+          </fieldset>
+        </div>
+      </div> 
+        
+        <input name="order-button" type="submit" />   
+      </form>
+  </div>
+  </div>
+  )
+}
 export default function App()  {
 
 
-
-const [values, setValues] = useState(initialFormValues)
 
 const [formErrors, setFormErrors] = useState(initialErrors)
 
@@ -96,139 +253,37 @@ const [formErrors, setFormErrors] = useState(initialErrors)
 //       .catch(() => setFormErrors({ ...formErrors, [evt.target.name]: errorMessage.errors[0]}))
 // }
 
-const onChange = evt => { 
-  // evt.persist();
-  //   validateChange(evt);
-  //   setValues({
-  //     ...values,
-  //     [evt.target.name]:
-  //       evt.target.type === "checkbox" ? evt.target.checked : evt.target.value,
-  //   });
-let { type, name, value, checked} = evt.target
+// const onChange = evt => { 
+//   // evt.persist();
+//   //   validateChange(evt);
+//   //   setValues({
+//   //     ...values,
+//   //     [evt.target.name]:
+//   //       evt.target.type === "checkbox" ? evt.target.checked : evt.target.value,
+//   //   });
+// let { type, name, value, checked} = evt.target
 
 // value = type === 'checkbox' ? checked : value
-console.log(name, value)
-setValues({...values, [name]: value })
-// console.log(evt)
+// console.log(name, value)
+// setValues({...values, [name]: value })
+// // console.log(evt)
 
-  // if (evt.target.id === 'name-input') {
-  //   setValues((prevState) =>({...prevState, username: evt.target.value }))
-  //   // validateChange(evt.target)
-  // }
-  // if (evt.target.id === 'size-dropdown') {
-  //   setValues((prevState) =>({...prevState, size: evt.target.value}))
-  // }
+//   // if (evt.target.id === 'name-input') {
+//   //   setValues((prevState) =>({...prevState, username: evt.target.value }))
+//   //   // validateChange(evt.target)
+//   // }
+//   // if (evt.target.id === 'size-dropdown') {
+//   //   setValues((prevState) =>({...prevState, size: evt.target.value}))
+//   // }
 
-  // if (evt.target.id ==='special-text') {
-  //   setValues((prevState) =>({...prevState, specialText: evt.target.value}))
-  // }
-  // setValues({...values, [name]: value })
+//   // if (evt.target.id ==='special-text') {
+//   //   setValues((prevState) =>({...prevState, specialText: evt.target.value}))
+//   // }
+//   // setValues({...values, [name]: value })
 
-  }
+//   }
 
-  function Order(props) {
-    const onSubmit = evt => {
-      evt.preventDefault()
-      axios.post('https://reqres.in/api/orders', values)
-        .then(response => {
-          console.log(response.data)
-        })
-        .catch(error => {
-          console.error(error)
-        })
-    }
-
-    return (
-     <div> 
-      <h2 style={{ ...style, borderColor: 'red'}}>Get yourself a pie.</h2>
-      
-      <div>
-        <h2>Build your own pizza here!</h2>
-        <p>{values.username}</p>
-        <form  onSubmit={onSubmit} id="pizza-form">
-          <div>
-            <label>Name for order: <br />
-              <input 
-                value = {values.username}
-                onChange = {onChange}
-                id="name-input" 
-                name="username" 
-                type="text" 
-                placeholder="Type Name" />
-                <br />
-            </label>
-          <div>
-            <label>Size of pizza: <br /></label>
-              <select
-                value = {values.size}
-                // onChange = {onChange}
-                id="size-dropdown" 
-                name="size-dropdown">
-                <option value="">-- Select Size --</option>
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>  
-              </select>
-          </div>
-          <div>
-            <label>
-              <input 
-              checked = {values.pepperoni}
-              // onChange = {onChange}
-              name = "pepperoni"
-              value = "pepperoni"
-              type="checkbox" />
-              Pepperoni
-            </label>
-            <label>
-              <input
-              checked = {values.mushrooms}
-              // onChange = {onChange}
-              name = "mushrooms"
-              value = "mushrooms" 
-              type="checkbox" />
-              Mushrooms
-            </label>
-            <label>
-              <input
-              checked = {values.onions}
-              // onChange = {onChange}
-              value = "onions"
-              name = "onions"
-              type="checkbox" />
-              Onions
-            </label>
-            <label>
-              <input
-              checked = {values.extra_cheese}
-              // onChange = {onChange} 
-              name = "extra_cheese"
-              value = "extra_cheese"
-              type="checkbox" />
-              Extra Cheese
-            </label> 
-          </div>
-          <div>
-            <label>Special Instructions: <br />
-              <input 
-                value = {values.specialText}
-                // onChange = {onChange}
-                id="special-text" 
-                name="special-text" 
-                type="text" 
-                placeholder="Special requests?" 
-                style={{width: '300px', height: "50px"}}/>
-                <br />
-            </label>
-          </div>
-        </div> 
-          
-          <input type="submit" />   
-        </form>
-    </div>
-    </div>
-    )
-  }
+ 
 
   return (
     <>
